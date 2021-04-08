30a0dfc21544fdbe0b09aaf754167892d2df1ae6
Manuel Casocavallo <manuelcasocavallo@gmail.com>
2021-04-08 17:52:17 +0200

    Initial Commit

---
+++ HWS-10-CupcakeCorner/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  HWS-10-CupcakeCorner
+//
+//  Created by Manuel Casocavallo on 08/04/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ HWS-10-CupcakeCorner/HWS_10_CupcakeCornerApp.swift
@@ -0,0 +1,17 @@
+//
+//  HWS_10_CupcakeCornerApp.swift
+//  HWS-10-CupcakeCorner
+//
+//  Created by Manuel Casocavallo on 08/04/21.
+//
+
+import SwiftUI
+
+@main
+struct HWS_10_CupcakeCornerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
