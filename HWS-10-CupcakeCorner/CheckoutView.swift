1020f5894dece069eb44f415b8509446dfdc5ca0
Manuel Casocavallo <manuelcasocavallo@gmail.com>
2021-04-09 16:33:41 +0200

    Final version

--- HWS-10-CupcakeCorner/CheckoutView.swift
+++ HWS-10-CupcakeCorner/CheckoutView.swift
@@ -9,6 +9,9 @@ import SwiftUI
 
 struct CheckoutView: View {
     @ObservedObject var order: Order
+    @State private var confirmationMessage = ""
+    @State private var showingConfirmation = false
+    
     var body: some View {
         GeometryReader { geometry in
             ScrollView {
@@ -21,14 +24,42 @@ struct CheckoutView: View {
                         .font(.title)
                     
                     Button("Place order") {
-                        //place the order
+                        placeOrder()
                     }
                     .padding()
                 }
             }
         }
         .navigationBarTitle("Check out", displayMode: .inline)
+        .alert(isPresented: $showingConfirmation) {
+            Alert(title: Text("Thank you!"), message: Text(confirmationMessage), dismissButton: .default(Text("OK")))
+        }
     }
+
+    func placeOrder() {
+        guard let encoded = try? JSONEncoder().encode(order) else {
+            print("Failed to encode order")
+            return
+        }
+        let url = URL(string: "https://reqres.in/api/cupcakes")!
+        var request = URLRequest(url: url)
+        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+        request.httpMethod = "POST"
+        request.httpBody = encoded
+        URLSession.shared.dataTask(with: request) { data, response, error in
+            guard let data = data else {
+                print("No data in response: \(error?.localizedDescription ?? "Unknown error")")
+                return
+            }
+            if let decodedOrder = try? JSONDecoder().decode(Order.self, from: data) {
+                confirmationMessage = "Your order for \(decodedOrder.quantity) \(Order.types[decodedOrder.type].lowercased()) cupcakes is on its way!"
+                showingConfirmation = true
+            } else {
+                print("Invalid response from server")
+            }
+        }.resume()
+    }
+    
 }
 
 struct CheckoutView_Previews: PreviewProvider {
--- HWS-10-CupcakeCorner/Order.swift
+++ HWS-10-CupcakeCorner/Order.swift
@@ -7,12 +7,52 @@
 
 import Foundation
 
-class Order: ObservableObject {
-    static let types = ["Vanilla", "Strawberry", "Chocolate", "Rainbow"]
+class Order: ObservableObject, Codable {
     
     @Published var type = 0
     @Published var quantity = 3
+    @Published var extraFrosting = false
+    @Published var addSprinkles = false
+    @Published var name = ""
+    @Published var streetAddress = ""
+    @Published var city = ""
+    @Published var zip = ""
     
+    //Create coding keys to conform to Codable
+    enum CodingKeys: CodingKey {
+        case type, quantity, extraFrosting, addSprinkles, name, streetAddress, city, zip
+    }
+    //Creating a container to encode using the coding keys
+    func encode(to encoder: Encoder) throws {
+        var container = encoder.container(keyedBy: CodingKeys.self)
+        try container.encode(type, forKey: .type)
+        try container.encode(quantity, forKey: .quantity)
+        try container.encode(extraFrosting, forKey: .extraFrosting)
+        try container.encode(addSprinkles, forKey: .addSprinkles)
+        try container.encode(name, forKey: .name)
+        try container.encode(streetAddress, forKey: .streetAddress)
+        try container.encode(city, forKey: .city)
+        try container.encode(zip, forKey: .zip)
+    }
+    //Implement a required initializer to decode an instance of Order
+    required init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        type = try container.decode(Int.self, forKey: .type)
+        quantity = try container.decode(Int.self, forKey: .quantity)
+        extraFrosting = try container.decode(Bool.self, forKey: .extraFrosting)
+        addSprinkles = try container.decode(Bool.self, forKey: .addSprinkles)
+        name = try container.decode(String.self, forKey: .name)
+        streetAddress = try container.decode(String.self, forKey: .streetAddress)
+        city = try container.decode(String.self, forKey: .city)
+        zip = try container.decode(String.self, forKey: .zip)
+    }
+    //Adding an alternative init
+    init() { }
+    
+    
+    
+    static let types = ["Vanilla", "Strawberry", "Chocolate", "Rainbow"]
+   
     @Published var specialRequestEnabled = false {
         didSet {
             if specialRequestEnabled == false {
@@ -21,13 +61,6 @@ class Order: ObservableObject {
             }
         }
     }
-    @Published var extraFrosting = false
-    @Published var addSprinkles = false
-    
-    @Published var name = ""
-    @Published var streetAddress = ""
-    @Published var city = ""
-    @Published var zip = ""
 
     var hasValidAddress : Bool {
         if name.isEmpty || streetAddress.isEmpty || city.isEmpty || zip.isEmpty {
